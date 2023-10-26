26b13e81f8e91f3241fab597c391b32177ef4ba2
OlenaLiekan <olena.liekan@gmail.com>
2023-10-26 17:20:25 +0300

    .

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -38,7 +38,7 @@ const SuccessPage = () => {
         <div className="main__success success-main">
             <div className="success-main__container">
                 <div className="success-main__content">
-                    {orderId
+                    {!orderId
                         ? 
                         <>
                             <h2 className="success-main__title">
@@ -110,15 +110,20 @@ const SuccessPage = () => {
                         </>
                         :
                         <>
-                            <p className="body-success__text">
-                                Esta página exibe informações para pagamento do pedido.   
-                            </p>
-                            <p className="body-success__text">
-                                Você ainda não tem um pedido.
-                            </p>
-                            <Link to="/" onClick={scrollTop} className="body-success__button go-shopping scroll-top">
-                                Voltar à página inicial
-                            </Link>
+                            <div className="success-main__body body-success">
+                                <p className="body-success__text">
+                                    Esta página exibe informações para pagamento do pedido.   
+                                </p>
+                                <p className="body-success__text">
+                                    Você ainda não tem um pedido. 
+                                </p>
+                                <svg className='body-success__svg' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                    <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z" />
+                                </svg>
+                                <Link to="/" onClick={scrollTop} className="body-success__button go-shopping scroll-top">
+                                    Voltar à página inicial
+                                </Link>
+                            </div>
                         </>
                     }
                 </div>
