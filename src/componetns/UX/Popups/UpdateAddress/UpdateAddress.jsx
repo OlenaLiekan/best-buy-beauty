0b676215c04b116f1c97d59f4169452ae7b197a7
Olena Liekan <olena.liekan@gmail.com>
2024-12-24 18:30:15 +0200

    .

--- src/componetns/UX/Popups/UpdateAddress/UpdateAddress.jsx
+++ src/componetns/UX/Popups/UpdateAddress/UpdateAddress.jsx
@@ -208,12 +208,12 @@ const UpdateAddress = ({userId, addressId, addresses, existingMainAddress}) => {
                     <input readOnly required onClick={showCountries} id="user-country-input" tabIndex="7" autoComplete="off" type="text" name="country" data-error="Error" className={styles.formInputSelect}
                         ref={inputRef}
                         value={countries.find((c) => c.code === country)?.name || ""}/>
-                    <svg onClick={showCountries} className={visibleList ? styles.rotate : ''} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                    <svg onClick={showCountries} className={visibleList ? styles.rotate : styles.arrow} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
                         <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" />
                     </svg>
                     <div className={visibleList ? styles.countriesWrapper : styles.hidden}>
                         <ul className={visibleList ? styles.countriesList : styles.hidden}>
-                                <li>
+                                <li className={styles.searchLine}>
                                     <input
                                         id="country-search-input"
                                         name="country_value"
@@ -223,6 +223,17 @@ const UpdateAddress = ({userId, addressId, addresses, existingMainAddress}) => {
                                         onChange={onChangeCountryValue}
                                         className={styles.countrySearch}
                                     />  
+                                    <svg
+                                        className={styles.searchSvg}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 512 512">
+                                        <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 
+                                        44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208
+                                        208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4
+                                        9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7
+                                        0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128
+                                        0 70.7-57.2 128-128 128z" />
+                                    </svg> 
                                 </li>
                                 {countryValue 
                                     ?
