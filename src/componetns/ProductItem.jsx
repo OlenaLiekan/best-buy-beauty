2dd576cef8e05c12bff280e32028eac7e2eea426
Olena Liekan <olena.liekan@gmail.com>
2026-03-04 21:45:17 +0200

    .

--- src/componetns/ProductItem.jsx
+++ src/componetns/ProductItem.jsx
@@ -293,10 +293,7 @@ const ProductItem = ({ obj, id, info, text, applying, compound, slide, typeId, r
                 if (kitId) {
                     if (firstSelectedOption && secondSelectedOption && thirdSelectedOption) {
                         const result = firstSelectedOption + secondSelectedOption + thirdSelectedOption;
-                        console.log(result);
-
-                        const selectedVariant = kitVariants.find((kitVariant) => kitVariant.variant.split(',').join('').split(' ').join('').toLowerCase() === result.split(' ').join('').toLowerCase());
-                        console.log(kitVariants.map((kitVariant) => kitVariant.variant));                        
+                        const selectedVariant = kitVariants.find((kitVariant) => kitVariant.variant.split(',').join('').split(' ').join('').toLowerCase() === result.split(' ').join('').toLowerCase());                  
                         if (selectedVariant) {
                             if (selectedVariant.id != id) {
                                 navigate(productType ? `/${camelize(productType.name)}/${selectedVariant.id}` : `/produtos/${selectedVariant.id}`);                    
